0ac404c907d39e31be887a4421c806aac65626dd
Emma Engstrom <engstrome48@gmail.com>
2023-01-23 10:46:54 -0700

    board array changed to char

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,54 +10,57 @@ namespace TicTacToe
             // welcome users
             Console.WriteLine("Welcome to Tic-Tac-Toe");
             // initialize array 
-            int[] board = new int[9];
+            char[] board = new char[" "," "," "," "," "," "," "," "," "];
             // loop through 9 guesses
             for (int i = 0; i < 8; i++)
             {
-                if board[i] != 0 && board[i] != 1{
                 // even numbers are player 1's guesses
                 if (i % 2 == 0)
                 {
                     Console.WriteLine("Player 1, it's your turn! Guess a number 1-9 (representing the tic-tac-toe squares): ");
                     temp = parse.Int(Console.ReadLine());
-                    board[temp] = 0
+                    if board[temp] != "X" && board[i] != "O" {
+                    board[temp] = "X";
                 }
+                                else
+                    {
+                Console.WriteLine("Sorry that square is already marked with " + board[i]);
+                i = i - 1;
+                    }
+
+                    }
                 else
                 {
                     Console.WriteLine("Player 2, it's your turn! Guess a number 1-9 (representing the tic-tac-toe squares): ");
                     temp = parse.Int(Console.ReadLine());
-                    board[temp] = 1
+                       if board[temp] != "X" && board[i] != "O" {
+                    board[temp] = "O";
                 }
-            else
+                                   else
                     {
-                Console.WriteLine("Sorry that square is already marked with " + parse.String(board[i]));
+                Console.WriteLine("Sorry that square is already marked with " + board[i]);
                 i = i - 1 
                     }
+
+
             }
-            // call the display board and check winner methods
-            //displayBoard(board);
-            //checkWinner(board);
+                            // call the display board and check winner methods
+            displayBoard();
+                
+            // check the string returned by checkWinner() and display to user 
+            string winString = checkWinner();
+                if winString != "No winner yet"{
+                Console.WriteLine(winString);
+                        }
+                else
+                {
+                    break;
+                }
 
-            // check the bools returned by checkWinner() and display to user 
-            //bool hasWinner, winnerIsPlayer1 = checkWinner();
+                }
 
-            //if (hasWinner == true)
-            //{
-              //  if (winnerIsPlayer1 == true)
-                //{
-                  //  Console.WriteLine("Player 1 wins!");
-                //}
-               // else
-                //{
-                  //  Console.WriteLine("Player 2 wins!");
-                //}
-            //}
-            //else
-            //{
-              //  Console.WriteLine("It was a tie.");
-           // }
-           // Console.WriteLine("Thanks for playing!")
-        }
+    
+            Console.WriteLine("Thanks for playing!")
 
     }
 }
