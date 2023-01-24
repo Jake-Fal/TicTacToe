21e7de5dcf7526423cc1f0bd47407fe201c1e388
Michael Chase <mchase62@byu.edu>
2023-01-24 09:34:50 -0700

    Support group coming in

---
+++ TicTacToe/GameBoard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+public class GameBoard
+{
+	public GameBoard(int[] newBoard) // constructor
+	{
+		int[] board = newBoard;
+	}
+
+	public void displayBoard()
+    {
+		Console.WriteLine('     |     |     ');
+		Console.WriteLine('  {0}  |  {1}  |  {2}', board[0], board[1], board[2]);
+		Console.WriteLine('_____|_____|_____');
+		Console.WriteLine('     |     |     ');
+		Console.WriteLine('\t|\t|\t');
+		Console.WriteLine('  {0}  |  {1}  |  {2}', board[3], board[4], board[5]);
+		Console.WriteLine('_____|_____|_____');	   			 		   
+		Console.WriteLine('     |     |     ');	   			 		   
+		Console.WriteLine('  {0}  |  {1}  |  {2}', board[6], board[7], board[8]);
+		Console.WriteLine('     |     |     ');
+	}
+
+	public int checkWinner()
+    {
+		char = winner;
+		
+		if (board[0] == board[1] && board[0] == board[2]) // check for 1st horizontal row
+		{
+			winner = board[0];
+        }
+		else if (board[3] == board[4] && board[5] == board[3]) // check for 2nd horizontal row
+        {
+			winner = board[3];
+		}
+		else if (board[6] == board[7] && board[6] == board[8]) // check for 3rd horizontal row
+		{
+			winner = board[6];
+		}
+		else if (board[0] == board[3] && board[0] == board[6]) // check for 1st vertical row
+		{
+			winner = board[0];
+		}
+		else if (board[1] == board[4] && board[7] == board[1]) // check for 2nd vertical row
+		{
+			winner = board[1];
+		}
+		else if (board[2] == board[5] && board[8] == board[3]) // check for 3rd vertical row
+		{
+			winner = board[2];
+		}
+		else if (board[0] == board[4] && board[8] == board[0]) // check for 1st diagonal row
+		{
+			winner = board[0];
+		}
+		else if (board[2] == board[4] && board[6] == board[2]) // check for 2nd diagonal row
+		{
+			winner = board[2];
+		}
+		else // if no winner has been found
+        {
+			if (board.AsQueryable().Any(val == ' ')) // there are still spaces on the board
+            {
+				winner = "No winner yet";
+            }
+			else // no more spaces
+            {
+				winner = "Cat's scratch";
+            }
+        }
+		if (winner == "X")
+        {
+			return "Player 1 won";
+        }
+		else if (winner == "O")
+        {
+			return "Player 2 won";
+        }
+	
+	}
+}
